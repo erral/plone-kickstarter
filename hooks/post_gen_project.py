90236a89f254896f00dbde6edab34f1aeece85b2
Jens W. Klein <jk@kleinundpartner.at>
2021-12-05 21:18:07 +0100

    a docker step

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 cwd = Path.cwd()
 
-if "{{ cookiecutter.mode }}" == "standalone":
+requirements_out = "{{ cookiecutter.requirements_out }}"
+if requirements_out != "requirements_barebone.txt":
     current_requirements_file = cwd / "requirements_barebone.txt"
-    renamed_requirements_file = cwd / "requirements.txt"
+    renamed_requirements_file = cwd / "{{ cookiecutter.requirements_out }}"
     current_requirements_file.rename(renamed_requirements_file)
